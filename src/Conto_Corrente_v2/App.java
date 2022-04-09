bdc7a9a277ab0031cee181b35f0cbe9dd60143e1
Triveeee <riccardo.trivellato2004@gmail.com>
2022-04-09 12:53:51 +0200

    modifica Conto_Corrente

--- src/Conto_Corrente_v2/App.java
+++ src/Conto_Corrente_v2/App.java
@@ -6,12 +6,14 @@ import javax.swing.*;
 public class App extends JFrame {
 
     public App(){
-        Conto_Corrente conto_corrente = new Conto_Corrente();
+
+        Conto_corrente conto_corrente = new Conto_corrente();
         this.setContentPane(new Panel_Conto(conto_corrente));
 
         this.setVisible(true);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLocationRelativeTo(null);
+        this.setResizable(false);
 
         this.pack();
     }
