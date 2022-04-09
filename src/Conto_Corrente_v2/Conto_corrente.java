532fb23046edf17c582b6bfea3a13862aa9e0e9b
Triveeee <riccardo,trivellato2004gmail.com>
2022-04-09 04:15:46 +0200

    aggiunta

--- src/Conto_Corrente_v2/Conto_corrente.java
+++ src/Conto_Corrente_v2/Conto_corrente.java
@@ -1,25 +1,26 @@
 package Conto_Corrente_v2;
 
-import javax.swing.JPanel;
+public class Conto_Corrente {
 
-public class Conto_corrente extends JPanel {
+    private double tot_conto;
 
-    private double tot_conto = 1000;
+    public Conto_Corrente(){
+        tot_conto = 1000;
+    }
 
+    public  double  movimento (double importo , boolean comand){
+        if (importo > tot_conto){
+            importo = tot_conto;
+        }
 
-
-    public synchronized void  movimento (double importo , boolean comand){
         if(comand == true){
             tot_conto = tot_conto - importo;
         }
-        if(comand == true){
+        if(comand == false){
             tot_conto = tot_conto + importo;
         }
-    }
-    
-    public synchronized double getTot_conto() {
+
         return tot_conto;
     }
-
 }
 
---
+++ src/Conto_Corrente_v2/Panel_Conto.java
@@ -0,0 +1,121 @@
+package Conto_Corrente_v2;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Panel_Conto extends JPanel implements Runnable {
+
+    private boolean comand;
+    private boolean cond;
+
+    private double tot_conto;
+    private JRadioButton b1;
+    private JRadioButton b2;
+    private JButton start;
+  
+    private JTextField txt1;
+
+    private JLabel lbl1;
+
+    private Thread t1;
+    private Thread t2;
+
+    private Conto_Corrente conto_corrente;
+
+    public Panel_Conto(Conto_Corrente conto_corrente){
+
+        this.conto_corrente = conto_corrente;
+
+        tot_conto = 1000;
+        comand = true;
+        cond = false;
+        
+        ButtonGroup group = new ButtonGroup();
+        b1 = new JRadioButton("prelevamento" , true);
+        b1.addActionListener(new GestioneBottone());
+        b2 = new JRadioButton("versamento");
+        b2.addActionListener(new GestioneBottone());
+        start = new JButton("start");
+        start.addActionListener(new GestioneBottone());
+
+        txt1 = new JTextField("inserisci importo" , 10);
+
+        lbl1 = new  JLabel("CONTO = " + tot_conto);
+
+        t1 = new Thread(this , "T1");
+        t2 = new Thread(this , "T2");
+
+        group.add(b1);
+        group.add(b2);
+
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        this.add(p1);
+        this.add(p2);
+
+        p1.add(txt1);
+        p1.add(b1);
+        p1.add(b2);
+
+        p2.add(lbl1);
+        p2.add(start);
+
+        t1.start();
+        t2.start();
+    }
+
+    public class GestioneBottone implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            //true --> prelevamento
+            //false --> versamento
+            if(e.getSource().equals(b1)){
+                comand = true;
+            }
+            if(e.getSource().equals(b2)){
+                comand = false;
+            }
+            if(e.getSource().equals(start)){
+                cond = true;
+            }
+            
+        }
+
+    }
+
+    @Override
+    public void run() {
+        // TODO Auto-generated method stub
+
+        while(true){
+            lbl1.setText("CONTO = " + tot_conto);
+
+            synchronized(conto_corrente){
+                try {
+                    if(cond == true){
+                        tot_conto = conto_corrente.movimento(Double.parseDouble(txt1.getText()) , comand);
+                        cond = false;
+                    }
+    
+                    Thread.sleep(100);
+    
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(this, "Errore" + e, "Errore", JOptionPane.ERROR_MESSAGE);
+                    cond = false;
+    
+                } catch (InterruptedException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            }
+        
+    }
+
+}
+
